6ebb87696bb6e1ad47d65454587f8cf2260114e0
Jan Soukup <soukup@u.plus>
2022-08-04 13:16:59 +0200

    feat(sharp): sharp layer added with build process

---
+++ build-sharp-layer.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+set -e
+
+LAMBDA_FOLDER=nodejs
+SHARP_IGNORE_GLOBAL_LIBVIPS=1
+
+npm install \
+    --arch=x64 \
+    --platform=linux \
+    --target=16.15 \
+    --libc=glibc \
+    --prefix=$LAMBDA_FOLDER \
+    sharp
+
+zip -r dist/sharp-layer.zip $LAMBDA_FOLDER
+
+rm -rf $LAMBDA_FOLDER
