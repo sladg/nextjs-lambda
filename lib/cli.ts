0407f40b1b00a7a0b4ef2fa5b3005b94482422de
Jan Soukup <soukup@u.plus>
2022-08-22 17:18:00 +0200

    fix: scripts

--- lib/cli.ts
+++ lib/cli.ts
@@ -1,15 +1,18 @@
 #!/usr/bin/env node
 import { exec as child_exec } from "child_process"
 import util from "util"
+import path from "path"
 
 const exec = util.promisify(child_exec)
 
 // @TODO: Ensure path exists.
 // @TODO: Ensure.next folder exists with standalone folder inside.
 
+const scriptName = path.resolve(`${__dirname}/../../scripts/pack-nextjs.sh`)
+
 const run = async () => {
   console.log("Starting packaging of your NextJS project!")
-  await exec("chmod +x ./pack-nextjs.sh && ./pack-nextjs.sh").catch(console.error)
+  await exec(`chmod +x ${scriptName} && ${scriptName}`).catch(console.error)
   console.log("Your NextJS project was succefully prepared for Lambda.")
 }
 
