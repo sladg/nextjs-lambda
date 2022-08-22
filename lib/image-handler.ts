bbe5b69899b95c9fcdf1fd8afc2f7f9b4617bccc
Jan <jan@ssoukup.com>
2022-08-22 17:00:26 +0200

    feat: server-handler and packaging for nextjs (#8)
    * feat: server-handler and packaging for nextjs
    
    * doc: readme heading improved
    
    Co-authored-by: Jan Soukup <soukup@u.plus>

--- lib/image-handler.ts
+++ lib/image-handler.ts
@@ -1,14 +1,13 @@
-// Ensure NODE_ENV is set to production
-process.env.NODE_ENV = 'production'
+process.env.NODE_ENV = "production"
 // Set NEXT_SHARP_PATH environment variable
 // ! Make sure this comes before the fist import
-process.env.NEXT_SHARP_PATH = require.resolve('sharp')
+process.env.NEXT_SHARP_PATH = require.resolve("sharp")
 
-import type { APIGatewayProxyEventV2, APIGatewayProxyStructuredResultV2 } from 'aws-lambda'
-import { defaultConfig, NextConfigComplete } from 'next/dist/server/config-shared'
-import { imageOptimizer as nextImageOptimizer, ImageOptimizerCache } from 'next/dist/server/image-optimizer'
-import { ImageConfigComplete } from 'next/dist/shared/lib/image-config'
-import { normalizeHeaders, requestHandler } from './utils'
+import type { APIGatewayProxyEventV2, APIGatewayProxyStructuredResultV2 } from "aws-lambda"
+import { defaultConfig, NextConfigComplete } from "next/dist/server/config-shared"
+import { imageOptimizer as nextImageOptimizer, ImageOptimizerCache } from "next/dist/server/image-optimizer"
+import { ImageConfigComplete } from "next/dist/shared/lib/image-config"
+import { normalizeHeaders, requestHandler } from "./utils"
 
 const sourceBucket = process.env.S3_SOURCE_BUCKET ?? undefined
 
@@ -29,12 +28,17 @@ const nextConfig = {
 const optimizer = async (event: APIGatewayProxyEventV2): Promise<APIGatewayProxyStructuredResultV2> => {
   try {
     if (!sourceBucket) {
-      throw new Error('Bucket name must be defined!')
+      throw new Error("Bucket name must be defined!")
     }
 
-    const imageParams = ImageOptimizerCache.validateParams({ headers: event.headers } as any, event.queryStringParameters!, nextConfig, false)
+    const imageParams = ImageOptimizerCache.validateParams(
+      { headers: event.headers } as any,
+      event.queryStringParameters!,
+      nextConfig,
+      false
+    )
 
-    if ('errorMessage' in imageParams) {
+    if ("errorMessage" in imageParams) {
       throw new Error(imageParams.errorMessage)
     }
 
@@ -43,16 +47,16 @@ const optimizer = async (event: APIGatewayProxyEventV2): Promise<APIGatewayProxy
       {} as any, // res object is not necessary as it's not actually used.
       imageParams,
       nextConfig,
-      requestHandler(sourceBucket),
+      requestHandler(sourceBucket)
     )
 
     console.log(optimizedResult)
 
     return {
       statusCode: 200,
-      body: optimizedResult.buffer.toString('base64'),
+      body: optimizedResult.buffer.toString("base64"),
       isBase64Encoded: true,
-      headers: { Vary: 'Accept', 'Content-Type': optimizedResult.contentType },
+      headers: { Vary: "Accept", "Content-Type": optimizedResult.contentType },
     }
   } catch (error: any) {
     console.error(error)
