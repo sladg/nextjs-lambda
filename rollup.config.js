3a5e0e4d9813734499ff6f3274bf65e19b015d02
Jan Soukup <soukup@u.plus>
2022-09-01 16:09:53 +0200

    ref(build): leaned into rollup, zipping done as part of rollup process

--- rollup.config.js
+++ rollup.config.js
@@ -1,29 +1,40 @@
 import pkg from './package.json'
 import ts from 'rollup-plugin-ts'
 import json from '@rollup/plugin-json'
+import cmd from 'rollup-plugin-command'
 
 export default [
 	{
 		input: 'lib/index.ts',
 		plugins: [json(), ts()],
-		output: [
-			{
-				file: pkg.main,
-				format: 'cjs',
-				file: 'dist/main.js',
-			},
-		],
+		output: {
+			format: 'cjs',
+			file: pkg.main,
+		},
 	},
 	{
 		input: 'lib/cli.ts',
 		plugins: [json(), ts()],
-		output: [
-			{
-				file: pkg.main,
-				format: 'cjs',
-				file: 'dist/cli.js',
-				banner: '#!/usr/bin/env node',
-			},
-		],
+		output: {
+			format: 'cjs',
+			file: pkg.bin.app,
+			banner: '#!/usr/bin/env node',
+		},
+	},
+	{
+		input: 'lib/standalone/image-handler.ts',
+		plugins: [ts(), cmd(`zip ${pkg.exports['image-handler/zip']} ${pkg.exports['image-handler']}`, { exitOnFail: true })],
+		output: {
+			format: 'cjs',
+			file: pkg.exports['image-handler'],
+		},
+	},
+	{
+		input: 'lib/standalone/server-handler.ts',
+		plugins: [ts(), cmd(`zip ${pkg.exports['server-handler/zip']} ${pkg.exports['server-handler']}`, { exitOnFail: true })],
+		output: {
+			format: 'cjs',
+			file: pkg.exports['server-handler'],
+		},
 	},
 ]
