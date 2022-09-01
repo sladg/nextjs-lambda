1e152916102618265191863f1adb60137fdb03c8
Jan <jan@ssoukup.com>
2022-09-01 16:59:01 +0200

    fix: export configuration, nested paths (#27)
    Co-authored-by: Jan Soukup <soukup@u.plus>

--- lib/consts.ts
+++ lib/consts.ts
@@ -1,5 +1,5 @@
 import packageJson from '../package.json'
 
-export const sharpLayerZipPath = require.resolve(packageJson.name + '/sharp-layer/zip')
-export const imageHandlerZipPath = require.resolve(packageJson.name + '/image-handler/zip')
-export const serverHandlerZipPath = require.resolve(packageJson.name + '/server-handler/zip')
+export const sharpLayerZipPath = require.resolve(packageJson.name + './sharp-layer/zip')
+export const imageHandlerZipPath = require.resolve(packageJson.name + './image-handler/zip')
+export const serverHandlerZipPath = require.resolve(packageJson.name + './server-handler/zip')
