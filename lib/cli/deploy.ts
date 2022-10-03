970f22938aae4ecbb398813d501b02d04fef9031
Bender <bender@bot.eu>
2022-10-03 16:15:25 +0200

    fix(swc): do not use swc on ts-node, historical typo fix

--- lib/cli/deploy.ts
+++ lib/cli/deploy.ts
@@ -12,7 +12,7 @@ export const deployHandler = async ({ stackName, tsconfigPath, appPath }: Props)
 	// Not using SWC as it's problematic on NPX (postinstall fails).
 	// NPX so ts-node does not need to be a dependency.
 	// All paths are absolute.
-	const cdkApp = `npx ts-node --swc --project ${tsconfigPath} ${appPath}`
+	const cdkApp = `npx ts-node --project ${tsconfigPath} ${appPath}`
 
 	await executeAsyncCmd({
 		cmd: `STACK_NAME=${stackName} ${cdkExecutable} deploy --app "${cdkApp}"`,
