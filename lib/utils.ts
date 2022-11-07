c03707e01710b540ffd20ac5a4e27cc0b8e42627
Bender <jan@ssoukup.com>
2022-11-07 10:50:27 +0100

    fix(changelog): correctly generate comparison URL for bitbucket

--- lib/utils.ts
+++ lib/utils.ts
@@ -306,7 +306,7 @@ export const getCommitLink = (remoteUrl: string, commit: string) => {
 
 export const getCompareLink = (remoteUrl: string, previous: string, next: string) => {
 	if (remoteUrl.includes('bitbucket.org')) {
-		const formattedPrevious = previous ? `${previous}%0D${next}` : next
+		const formattedPrevious = previous ? `${next}%0D${previous}` : next
 		return `${remoteUrl}/branches/compare/${formattedPrevious}`
 	}
 
