a5161eb430aec6f2febaf30ab962e7dbe9c67a25
John Tipper <John_Tipper@hotmail.com>
2022-11-23 00:09:41 +0000

    #56 Allow an API route to return binary data by not hardcoding the response as non-binary.

--- lib/standalone/server-handler.ts
+++ lib/standalone/server-handler.ts
@@ -36,7 +36,10 @@ const server = slsHttp(
 	},
 	{
 		// We have separate function for handling images. Assets are handled by S3.
-		binary: false,
+		// If you wish to return binary data from an API route then set process.env.BINARY_CONTENT_TYPES to be a
+		// comma-separated list of Content-Types to be treated as binary data.
+		// c.f. https://github.com/dougmoscrop/serverless-http/blob/master/lib/provider/aws/is-binary.js for details on
+		// how this works.
 		provider: 'aws',
 		basePath: process.env.NEXTJS_LAMBDA_BASE_PATH,
 	},
